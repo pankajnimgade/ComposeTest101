8b91d5b9d9da521d862abc5eabfff8876d2190f4
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2021-10-31 16:27:32 -0500

    updated dark view

--- app/src/main/java/com/example/test101/compose104/Compose104Activity.kt
+++ app/src/main/java/com/example/test101/compose104/Compose104Activity.kt
@@ -1,38 +1,100 @@
 package com.example.test101.compose104
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.test101.compose104.ui.theme.Test101Theme
+import androidx.compose.ui.unit.dp
+import com.example.test101.R
+import com.example.test101.compose103.Message
+import com.example.test101.compose104.ui.theme.ComposeTutorialTheme104Theme
 
 class Compose104Activity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Test101Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting4("Android")
-                }
+            ComposeTutorialTheme104Theme {
+                MessageCardWithPicture(
+                    message = Message(
+                        author = "Android",
+                        body = "Jetpack Compose"
+                    )
+                )
             }
         }
     }
 }
 
+private data class Message(val author: String, val body: String)
+
 @Composable
-fun Greeting4(name: String) {
-    Text(text = "Hello $name!")
+private fun MessageCardWithPicture(message: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(4.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = message.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 4.dp) {
+                Text(
+                    text = message.body,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
+@Preview(
+    showBackground = true,
+    name = "Light Mode"
+)
 @Composable
-fun DefaultPreview4() {
-    Test101Theme {
-        Greeting4("Android")
+fun PreviewMessageCardWithPicture() {
+    ComposeTutorialTheme104Theme {
+        MessageCardWithPicture(message = Message(author = "Android", body = "Jetpack Compose"))
+    }
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewNightMessageCardWithPicture() {
+    ComposeTutorialTheme104Theme {
+        MessageCardWithPicture(message = Message(author = "Android", body = "Jetpack Compose"))
     }
 }
--- app/src/main/java/com/example/test101/compose104/ui/theme/Theme.kt
+++ app/src/main/java/com/example/test101/compose104/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun Test101Theme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun ComposeTutorialTheme104Theme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
